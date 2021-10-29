ea94e48da7059d9be5c0898ffc9a41c22a633eca
Duy T Danh <42757107+DuyDanh@users.noreply.github.com>
2021-10-29 14:25:59 -0400

    fix MONGOD_URI

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const expressHandlebars = require('express-handlebars');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB || 'mongodb://localhost/DomoMaker';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 mongoose.connect(dbURL, (err) => {
   if (err) {
